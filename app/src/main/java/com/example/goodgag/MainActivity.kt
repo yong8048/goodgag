8362ad7270f9348d6b16fd027397f45f5cd4f6c1
bwlee <lbw3973@naver.com>
2022-06-11 22:45:05 +0900

    bwlee
    BottomNavigationBar 주석하고 Button으로 변경 (Test)

--- app/src/main/java/com/example/goodgag/MainActivity.kt
+++ app/src/main/java/com/example/goodgag/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.goodgag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnBack = findViewById<Button>(R.id.btnBack);
+
+        btnBack.setOnClickListener{
+            x();
+        }
+    }
+
+    fun x(){
     }
 }
