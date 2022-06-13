2bb7f01a2a8c9f6fe5f239b1fecf0526fe9f5203
yong8048 <water8048@naver.com>
2022-06-14 01:20:12 +0900

    보거라

--- app/src/main/java/com/example/goodgag/MainActivity.kt
+++ app/src/main/java/com/example/goodgag/MainActivity.kt
@@ -12,17 +12,17 @@ class MainActivity : AppCompatActivity() {
 
     var postList = arrayListOf<Post>(
         Post("1", "이승용", "2022.123"),
-        Post("2", "이병욱", "2022./23"),
-        Post("3", "이승용", "2022.123"),
-        Post("4", "이병욱", "2022./23"),
-        Post("5", "이승용", "2022.123"),
-        Post("6", "이병욱", "2022./23"),
-        Post("7", "이승용", "2022.123"),
-        Post("8", "이병욱", "2022./23"),
-        Post("9", "이승용", "2022.123"),
-        Post("10", "이병욱", "2022./23"),
-        Post("11", "이승용", "2022.123"),
-        Post("12", "이병욱", "2022./23"),
+        Post("2", "오근혁", "2022./23"),
+        Post("3", "조명철", "2022.123"),
+        Post("4", "오근혁", "2022./23"),
+        Post("5", "조명철", "2022.123"),
+        Post("6", "오근혁", "2022./23"),
+        Post("7", "조명철", "2022.123"),
+        Post("8", "오근혁", "2022./23"),
+        Post("9", "조명철", "2022.123"),
+        Post("10", "조명철", "2022./23"),
+        Post("11", "조명철", "2022.123"),
+        Post("12", "조명철", "2022./23"),
         Post("13", "이승용", "2022.123"),
         Post("14", "이병욱", "2022./23"),
         Post("15", "이승용", "2022.123"),
@@ -59,10 +59,10 @@ class MainActivity : AppCompatActivity() {
     inner class SimpleListListener : AdapterView.OnItemClickListener{
         override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
             if(b){
-                tv_Main.text = "이승용볍신ㅋㅋ"
+                tv_Main.text = "조명철볍신ㅋㅋ"
             }
             else{
-                tv_Main.text = "ㅋㅋ볍신이승용"
+                tv_Main.text = "ㅋㅋ볍신조명철"
             }
             b = !b
         }
