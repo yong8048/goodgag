688523272f32564d1c7531669e797eccd53f55b5
yong8048 <water8048@naver.com>
2022-06-16 03:17:17 +0900

    어댑터클래스 지우고 해놧엉

--- app/src/main/java/com/example/goodgag/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/goodgag/activity/SignUpActivity.kt
@@ -1,13 +1,9 @@
 package com.example.goodgag.activity
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.goodgag.R
-import com.example.goodgag.adapter.SignUpSpinnerAdapter
-import com.google.firebase.*
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity :AppCompatActivity(){
@@ -16,10 +12,9 @@ class SignUpActivity :AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-//        var sData = resources.getStringArray(R.array.mails)
-//        var adapter = ArrayAdapter<String>(this,R.layout.activity_signup,sData)
-//        spMailList.adapter = adapter
+        var mailData = resources.getStringArray(R.array.mails)
+        spMailList.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mailData)
 
-//        spMailList.setSelection(1)
+
     }
 }
