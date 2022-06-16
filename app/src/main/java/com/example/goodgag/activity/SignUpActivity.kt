cbe5ec1e41dd5580c8b1d943217ad71ebaa09a13
bwlee <lbw3973@naver.com>
2022-06-16 17:31:31 +0900

    회원가입 할때, EditText Null 체크

--- app/src/main/java/com/example/goodgag/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/goodgag/activity/SignUpActivity.kt
@@ -30,14 +30,43 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun Click_SignUp(view: View) {
-        firebasepath!!.createUserWithEmailAndPassword(et_email.text.toString().trim() + spMailList.selectedItem.toString().trim(), et_pw.text.toString().trim()).addOnCompleteListener(this) {
-            if (it.isSuccessful) {
-                val user = firebasepath?.currentUser
-                Toast.makeText(this, "Authentication Success.", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Authentication Fail.", Toast.LENGTH_SHORT).show()
+        var toastMessage: String = ""
+        if(etName.text.toString() == ""){
+            toastMessage += "Name, "
+        }
+        if (etEmail.text.toString() == "") {
+            toastMessage += "Email, "
+        }
+        if(etPW.text.toString() == ""){
+            toastMessage += "PassWord, "
+        }
+        if(etPWcheck.text.toString() == ""){
+            toastMessage += "PassWord Check, "
+        }
+        if(etPhone1.text.toString() == "" || etPhone2.text.toString() == "" || etPhone3.text.toString() == ""){
+            toastMessage += "Phone Number, "
+        }
+        if(etBirth.text.toString() == ""){
+            toastMessage += "BirthDay, "
+        }
+        if(toastMessage == "") {
+            firebasepath!!.createUserWithEmailAndPassword(
+                etEmail.text.toString().trim() + spMailList.selectedItem.toString().trim(),
+                etPW.text.toString().trim()
+            ).addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val user = firebasepath?.currentUser
+                    toastMessage = "Authentication Success."
+                }
+                else {
+                    toastMessage = "Authentication Fail!"
+                }
+                Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
             }
         }
-
+        else{
+            toastMessage = toastMessage.substring(0, toastMessage.length - 2)
+            Toast.makeText(this, "Empty : $toastMessage", Toast.LENGTH_SHORT).show()
+        }
     }
 }
